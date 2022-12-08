5bad38a0e56c87f593dcd604710d49c78444b1fe
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-12-07 21:27:51 -0600

    iniciocarousel

--- src/components/pages/Inicio.vue
+++ src/components/pages/Inicio.vue
@@ -1,13 +1,80 @@
 <template>
-  <div class="container">
-    <!--Animated title-->
-    <div class="animate-div">
-      <p class="animate__animated animate__slideInDown">Inicio</p>
+  <div class="container py-5">
+    <div
+      id="carouselExampleIndicators"
+      class="carousel slide carousel-dark my-shadow"
+      data-bs-ride="true"
+    >
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="3000">
+          <router-link to="/contacto">
+            <img
+              src="https://picsum.photos/id/302/600/200"
+              class="d-block w-100"
+              alt="..."
+            />
+          </router-link>
+        </div>
+        <div class="carousel-item" data-bs-interval="3000">
+          <router-link to="/contacto">
+            <img
+              src="https://picsum.photos/id/404/600/200"
+              class="d-block w-100"
+              alt="..."
+            />
+          </router-link>
+        </div>
+        <div class="carousel-item" data-bs-interval="3000">
+          <router-link to="/contacto">
+            <img
+              src="https://picsum.photos/id/507/600/200"
+              class="d-block w-100"
+              alt="..."
+            />
+          </router-link>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Anterior</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Siguiente</span>
+      </button>
     </div>
-    <!--Animated title end-->
-    <br />
-    <router-link to="/template"> Template </router-link>
-    <br />
   </div>
 </template>
 
