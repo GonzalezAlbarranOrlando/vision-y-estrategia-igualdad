53e4a1af489862700e381c7a5775bfb1f4664ab2
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2022-12-12 11:47:54 -0600

    images

--- src/components/pages/Inicio.vue
+++ src/components/pages/Inicio.vue
@@ -29,18 +29,18 @@
       </div>
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="3000">
-          <router-link to="/contacto">
+          <router-link to="/NMX_025">
             <img
-              src="https://picsum.photos/id/302/600/200"
+              src="@/assets/img/carrusel/NMX025.jpeg"
               class="d-block w-100"
               alt="..."
             />
           </router-link>
         </div>
         <div class="carousel-item" data-bs-interval="3000">
-          <router-link to="/contacto">
+          <router-link to="/Perspectiva_de_g%C3%A9nero">
             <img
-              src="https://picsum.photos/id/404/600/200"
+              src="@/assets/img/carrusel/perspectiva.jpeg"
               class="d-block w-100"
               alt="..."
             />
@@ -74,6 +74,60 @@
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Siguiente</span>
       </button>
+    </div>
+    <h1 class="my-h1 mt-5">Servicios</h1>
+    <div class="row justify-content-center" id="servicios">
+      <div class="col-sm-6 py-2">
+        <!--card servicios-->
+        <div class="card">
+          <router-link to="/NMX_025" style="text-decoration: none">
+            <img
+              src="@/assets/img/inicio/NMX025.jpeg"
+              class="w-100"
+              alt="..."
+            />
+            <div
+              class="
+                card-header
+                table-title
+                d-flex
+                align-items-center
+                justify-content-center
+              "
+            >
+              <h5 class="my-3">Norma Mexicana NMX 025</h5>
+            </div>
+          </router-link>
+        </div>
+        <!--card servicios end-->
+      </div>
+      <div class="col-sm-6 py-2">
+        <!--card servicios-->
+        <div class="card">
+          <router-link
+            to="/Perspectiva_de_g%C3%A9nero"
+            style="text-decoration: none"
+          >
+            <img
+              src="@/assets/img/inicio/perspectiva.jpeg"
+              class="w-100"
+              alt="..."
+            />
+            <div
+              class="
+                card-header
+                table-title
+                d-flex
+                align-items-center
+                justify-content-center
+              "
+            >
+              <h5 class="my-3">Perspectiva de Género</h5>
+            </div>
+          </router-link>
+        </div>
+        <!--card servicios end-->
+      </div>
     </div>
   </div>
 </template>
